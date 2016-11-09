240bd43bef13eb7cd550afbebb79c31a575cffe9
tillrose <rose@pflanzenbau.uni-kiel.de>
2016-11-09 09:15:04 +0100

    Initial Commit

---
+++ LookupHUME.R
@@ -0,0 +1,18 @@
+
+## Lookup-Tables for TransHUME
+
+unit_lookup <- c("EC" = "[]",
+                 "Time" = "[d]",
+                 "LAI" = "[m2/m2]",
+                 "CumCarbo" = "[g/m2]",
+                 "avSLA" = "[squarecm/g]",
+                 "fStem" = "[-]")
+
+submodel_lookup <- c("EC" = "Development1",
+                     "LAI" = "SubLeafAreaSimple1",
+                     "CumCarbo" = "SubDryMatterSimple1",
+                     "avSLA" = "SubLeafAreaSimple1",
+                     "fStem" = "SubPartitioningSimple1")
+
+
+
